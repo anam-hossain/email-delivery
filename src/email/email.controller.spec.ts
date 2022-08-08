8368ce0dfd597f3a3a47528f7ff389bc23984f55
Anam Hossain <anam.hossain@theiconic.com.au>
2022-08-08 13:02:06 +1000

    Add email controller tests

--- src/email/email.controller.spec.ts
+++ src/email/email.controller.spec.ts
@@ -1,18 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EmailController } from './email.controller';
+import { EmailService } from '@/email/email.service';
+import { SendEmailDto } from '@/email/dto/send-email.dto';
 
 describe('EmailController', () => {
+  let emailService: EmailService;
   let controller: EmailController;
+  const mockResponse = {
+    json: jest.fn().mockReturnThis(),
+    status: jest.fn().mockReturnThis(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: EmailService,
+          useValue: {
+            send: jest.fn(),
+          },
+        },
+      ],
       controllers: [EmailController],
     }).compile();
 
     controller = module.get<EmailController>(EmailController);
+    emailService = module.get<EmailService>(EmailService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should send the email', async () => {
+    const emailServiceSpy = jest.spyOn(emailService, 'send');
+    const res = await controller.send(new SendEmailDto(), mockResponse);
+
+    expect(emailServiceSpy).toBeCalledTimes(1);
   });
 });
